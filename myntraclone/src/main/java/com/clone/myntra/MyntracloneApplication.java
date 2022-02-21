c51ba1bc7ef137735af258fb55048b978cf7c892
Dinesh Musali <dineshmusali12@gmail.com>
2022-02-21 11:04:18 +0530

    first commit

---
+++ myntraclone/src/main/java/com/clone/myntra/MyntracloneApplication.java
@@ -0,0 +1,13 @@
+package com.clone.myntra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyntracloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyntracloneApplication.class, args);
+	}
+
+}
